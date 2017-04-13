bb72d5ee2d81832bbd62b3b662e2c3f341694480
daniel <daniel.alvarado@overlapcr.com>
2017-04-13 09:25:56 -0600

    Ruta por defecto (root)

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/ForoFEUCR/', to: 'congresses#index'
+
+  root 'congresses#index'
 end
