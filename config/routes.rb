2ae705fcd597aee47520929f3878fceaa52adef4
Ariel Gonzalo <ariel.gonzalo@overlapcr.com>
2017-04-12 19:07:12 -0600

    routes.rb modificado

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   resources :discussions
   resources :congresses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/ForoFEUCR/', to: 'Congresses#index'
 end
