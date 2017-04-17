41ba5017931fb4465bd172256aba8390ee68d4d9
daniel <daniel.alvarado@overlapcr.com>
2017-04-16 18:47:11 -0600

    paperclip funcionando en presentations

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -3,13 +3,10 @@ class Presentation < ApplicationRecord
   has_one :user, :foreign_key => 'uploader'
   has_one :discussion
   has_many :presenters
-  has_many :users, :foreign_key => 'likes', optional: true
-  has_many :users, :foreign_key => 'dislikes', optional: true
-
-  #attr_accessible :pdf_file
+  has_many :users, :foreign_key => 'dislikes'
+  
   has_attached_file :pdf_file
   validates_attachment_content_type :pdf_file, :content_type => ["application/pdf"]
-  #validates :pdf_file, :attachment_presence => true
 
   #belongs_to :category
 end
