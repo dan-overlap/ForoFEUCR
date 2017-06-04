11247437c7cdba40e844b301c87b92fc8bdcb91b
Ariel Gonzalo <ariel.gonzalo@overlapcr.com>
2017-06-03 19:25:41 -0600

    Cambios en view de Congress

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170603025006) do
+ActiveRecord::Schema.define(version: 20170603034205) do
 
   create_table "administrations", force: :cascade do |t|
     t.integer  "congress_id"
@@ -185,7 +185,7 @@ ActiveRecord::Schema.define(version: 20170603025006) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "username"
+    t.string   "username",                            null: false
     t.string   "user_id",                             null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
