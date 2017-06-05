2c5b6cc825e0e615ce97fe54370e328b2bc5b862
Ariel Gonzalo <ariel.gonzalo@overlapcr.com>
2017-06-04 18:41:35 -0600

    Cambios minimos

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user,  :foreign_key  => 'author_id'
-  has_many :comments, :foreign_key => 'replies'
+  has_many :replies
+  has_many :comments, :through => :replies
   has_many :users, :foreign_key => 'likes'
   has_many :users, :foreign_key => 'dislikes'
   belongs_to :presentation, optional: true
-  belongs_to :comment, optional: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170603034205) do
+ActiveRecord::Schema.define(version: 20170605002330) do
 
   create_table "administrations", force: :cascade do |t|
     t.integer  "congress_id"
@@ -172,6 +172,15 @@ ActiveRecord::Schema.define(version: 20170603034205) do
     t.index ["uploader_id"], name: "index_presentations_on_uploader_id"
   end
 
+  create_table "replies", force: :cascade do |t|
+    t.integer  "comment_id"
+    t.integer  "reply_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["comment_id"], name: "index_replies_on_comment_id"
+    t.index ["reply_id"], name: "index_replies_on_reply_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -185,7 +194,7 @@ ActiveRecord::Schema.define(version: 20170603034205) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "username"
+    t.string   "username",                            null: false
     t.string   "user_id",                             null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
