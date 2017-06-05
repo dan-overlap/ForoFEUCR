479426d0d05fa89636bf4a779b2d9fbe31cd3c54
Ariel Gonzalo <ariel.gonzalo@overlapcr.com>
2017-06-04 19:15:52 -0600

    Cambios minimos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
 
 # saves the location before loading each page so we can return to the
 # right page. If we're on a devise page, we don't want to store that as the
 # place to return to (for example, we don't want to return to the sign in page
 # after signing in), which is what the :unless prevents
 before_action :store_current_location, :unless => :devise_controller?
+after_action  :store_location
+
 
 private
   # override the devise helper to store the current location so we can
@@ -15,15 +16,40 @@ private
     store_location_for(:user, request.url)
   end
 
+  def store_location
+  # store last url - this is needed for post-login redirect to whatever the user last visited.
+  return unless request.get? 
+  if (request.path != "/users/sign_in" &&
+    request.path != "/users/sign_up" &&
+    request.path != "/users/password/new" &&
+    request.path != "/users/password/edit" &&
+    request.path != "/users/confirmation" &&
+    request.path != "/users/sign_out" &&
+      !request.xhr?) # don't store ajax calls
+  session[:previous_url] = request.fullpath 
+end
+end
 
-  # override the devise method for where to go after signing out because theirs
-  # always goes to the root path. Because devise uses a session variable and
-  # the session is destroyed on log out, we need to use request.referrer
-  # root_path is there as a backup
-  def after_sign_out_path_for(resource)
-    request.referrer || root_path
-  end
+def store_location
+  # store last url - this is needed for post-login redirect to whatever the user last visited.
+  return unless request.get? 
+  if (request.path != "/users/sign_in" &&
+    request.path != "/users/sign_up" &&
+    request.path != "/users/password/new" &&
+    request.path != "/users/password/edit" &&
+    request.path != "/users/confirmation" &&
+    request.path != "/users/sign_out" &&
+      !request.xhr?) # don't store ajax calls
+  session[:previous_url] = request.fullpath 
+end
+end
 
-  
+def after_sign_in_path_for(resource)
+  session[:previous_url] || root_path
+end
+
+def after_sign_out_path_for(resource_or_scope)
+  request.referrer
+end
 
 end
