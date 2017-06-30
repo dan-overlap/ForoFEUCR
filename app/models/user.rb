142af4c3c433f024174a6d06364447a286995df3
Ariel Gonzalo <ariel.gonzalo@overlapcr.com>
2017-06-30 06:06:57 -0600

    registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
 
-         :recoverable, :rememberable, :trackable#, :validatable, :confirmable
-  #validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@ucr\.ac\.cr\z/, message: ": Por favor ingrese con su correo oficial de la UCR" }
+         :recoverable, :rememberable, :trackable, :validatable#, :confirmable
+  validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@ucr\.ac\.cr\z/, message: ": Por favor ingrese con su correo oficial de la UCR" }
 
   has_many :presentations
   has_many :administrations
