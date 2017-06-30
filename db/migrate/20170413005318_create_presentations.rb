d06978b000b9e282857b37bddc80e8f4397128b0
Ariel Gonzalo <ariel.gonzalo@overlapcr.com>
2017-06-30 05:47:47 -0600

    registration

--- db/migrate/20170413005318_create_presentations.rb
+++ db/migrate/20170413005318_create_presentations.rb
@@ -1,7 +1,7 @@
 class CreatePresentations < ActiveRecord::Migration[5.0]
   def change
     create_table :presentations do |t|
-      t.references :authors, foreign_key: true
+      t.integer :authors
       t.string :title
       t.string :abstract
       t.references :uploader, foreign_key: true
