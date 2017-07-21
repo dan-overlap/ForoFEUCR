f42f3fe8e4b5409fe604a683432bb01b8e4cf8d1
Ariel Gonzalo <ariel.gonzalo@overlapcr.com>
2017-07-20 20:06:25 -0600

    Allow iframe

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,10 @@ module ForoFEUCR
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.action_dispatch.default_headers = {
+    	'X-Frame-Options' => 'ALLOWALL'
+	}
     
   end
 
