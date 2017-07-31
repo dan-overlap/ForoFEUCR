ed4afb11e80b43003a4301e6d66fe9069b502a03
Ariel Gonzalo <ariel.gonzalo@overlapcr.com>
2017-07-31 12:29:15 -0600

    Comment delete

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module ForoFEUCR
   class Application < Rails::Application
   	config.assets.paths << "#{Rails}/vendor/assets/fonts"
   	config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile += %w(*.js)
   	config.action_view.embed_authenticity_token_in_remote_forms = true
   	
     # Settings in config/environments/* take precedence over those specified here.
